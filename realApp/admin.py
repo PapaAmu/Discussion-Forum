91933cfc1bee0a32d5c4333641136d33b0a1afdb
Themba Real Lukhele <realthemba@gmail.com>
2022-08-12 03:39:26 +0200

    registration...admin

--- realApp/admin.py
+++ realApp/admin.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-
 # Register your models here.
 
 from django.contrib import admin
-from .models importDiscussion, 
+from .models import * 
 
 #your model will be registered right here
 admin.site.register(forum)
