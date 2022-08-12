e2e9674de39c3baaf0602485d3afed3077bc40d1
Themba Real Lukhele <realthemba@gmail.com>
2022-08-12 05:10:20 +0200

    bug fixing

--- realApp/form.py
+++ realApp/form.py
@@ -1,7 +1,5 @@
 #Creating and updating models
 
-from dataclasses import field
-from inspect import formatargvalues
 from django.forms import ModelForm
 from .models import *
 
@@ -12,5 +10,5 @@ class CreateInForum(ModelForm):
 
 class CreateInDiscussion(ModelForm):
     class Meta:
-        model = Discussion 
+        model = Discussion
         fields ="__all__"
