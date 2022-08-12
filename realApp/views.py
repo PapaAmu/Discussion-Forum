1db01d4a2203bb131390a0bcfad12aaf27ae008e
Themba Real Lukhele <realthemba@gmail.com>
2022-08-12 04:12:54 +0200

    view set up file

--- realApp/views.py
+++ realApp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from itertools import count
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import *
 
 # Create your views here.
+
+def home(request):
+    forums = forum.objects.all()
+    count = forum.count()
+    discussion = []
+    for i in forum:
+        discussion.append(i.discussion_set.all())
+
+    context = {'forums':forums,
+                'count':count,
+                'discussions':discussions}
+    return render(request,'home.html',context)
+    
