a24312aaaa4d9544d715ac4af72ff514c7df701d
Themba Real Lukhele <realthemba@gmail.com>
2022-08-12 02:27:55 +0200

    I am creating a database on models.py file

--- realApp/models.py
+++ realApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+#parent model
+class forum(models.Model):
+
+
+
+
+    def __str__(self):
+        return str(self.topic)
+
+    
+
+#child model
+class Discussion(models.Model):
+    
+
+    def __str__(self):
+        return str(self.forum)
+
+
+
+
+
+        
