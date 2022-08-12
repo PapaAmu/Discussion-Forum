72d278f54550de604ce5ae2ed71a332e684674e0
Themba Real Lukhele <realthemba@gmail.com>
2022-08-12 04:24:09 +0200

    fixing code

--- realApp/views.py
+++ realApp/views.py
@@ -1,4 +1,3 @@
-from itertools import count
 from django.shortcuts import render,redirect
 from .models import *
 from .forms import *
@@ -17,3 +16,23 @@ def home(request):
                 'discussions':discussions}
     return render(request,'home.html',context)
     
+
+def addInForum(request):
+    form = CreateInForum()
+    if request.method == 'POST':
+        form = CreateInForum(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request, 'addInForum.html', context)
+
+def addInDiscussion(request):
+    form = CreateInDiscussion()
+    if request.method == 'POST':
+        form = CreateInDiscussion(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request,'addInDiscussion.html', context)
