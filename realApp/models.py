03439826a824be8cd23c8ec95585653b3bf8eef4
Themba Real Lukhele <realthemba@gmail.com>
2022-08-12 02:56:57 +0200

    working on my class, changin default behaviour of django

--- realApp/models.py
+++ realApp/models.py
@@ -1,3 +1,4 @@
+import email
 from django.db import models
 
 # Create your models here.
@@ -5,7 +6,12 @@ from django.db import models
 
 #parent model
 class forum(models.Model):
-
+    name = models.CharField(max_length=200, default = "anonymous" )
+    email = models.CharField(max_length=200, null=True)
+    topic = models.CharField(max_length=300)
+    description = models.CharField(max_length=1000, blank=True)
+    link = models.CharField(max_length=100, null =  True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 
 
